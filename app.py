807f7b853f7cbec33d3a8fec013b852e32ac2792
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2023-04-21 09:34:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,23 +29,7 @@ def generate_key_points(data):
         key_points.append(summary)
     return key_points
 
-# Define function to generate questions using GPT-3
-def generate_qa(key_points):
-    qa_pairs = []
-    for kp in key_points:
-        prompt = "Please generate a question and answer based on the following key point:\n" + kp
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            timeout=30,
-            )
-        qa = response.choices[0].text.strip().split("\n")
-        qa_pairs.append((qa[0], qa[1]))
-    return qa_pairs
+
 
 def main():
     st.title('Tabular Data Key Points Generator')
@@ -63,14 +47,7 @@ def main():
         st.subheader('Key Points')
         st.write(key_points)
         
-        # Generate questions and answers
-        qa_pairs = generate_qa(key_points)
         
-        # Show questions and answers
-        st.subheader('Questions and Answers')
-        for i, qa in enumerate(qa_pairs):
-            st.write(f"{i+1}. Q: {qa[0]}")
-            st.write(f"   A: {qa[1]}")
 
         
 if __name__ == '__main__':
